a762276a70e10e3da094d77abf0944e72bba3c70
Bence Pentek <pntkbence@gmail.com>
2022-09-15 09:16:04 +0200

    upload new images

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -4,17 +4,22 @@ import UploadImage from './UploadImage';
 
 const Gallery = () => {
   const data = dummyData;
+
+  let newObject = [localStorage.getItem('gallery')];
+  let storedImages = [...newObject];
+  console.log(storedImages)
+  data.push(JSON.parse(storedImages));
+
   function openModal() {
     document.getElementById('myModal').style.display = 'block';
-    // document.getElementById('img').style.transfrom = 'scale(1.5)';
-    // document.getElementById(`${img}`).style.width = '100px';
-    // document.getElementById('mySlides').style.display = 'block';
   }
+
   function closeModal() {
     document.getElementById('myModal').style.display = 'none';
   }
 
   const renderImg = data.map((img, i) => {
+    // console.log(img);
     return (
       <div key={i}>
         <img id={i} src={img.src} alt={img.alt} className={classes.hoverShadow} onClick={openModal} />
@@ -31,6 +36,7 @@ const Gallery = () => {
         <span className={classes.close} onClick={closeModal}>
           &times;
         </span>
+        <div className={classes.mySlides}>{renderImg}</div>
       </div>
     </div>
   );
--- src/components/Gallery/UploadImage.js
+++ src/components/Gallery/UploadImage.js
@@ -1,5 +1,45 @@
 import { useState } from 'react';
 
-const UploadImage = () => {};
+import dummyData from './dummyData.json';
+
+const UploadImage = () => {
+  //   const [title, setTitle] = useState('');
+  const [imgUrl, setImgUrl] = useState('');
+  const [imgName, setImgName] = useState('');
+
+  let jsonStr = dummyData;
+  let imageId = Math.random() + 1;
+  //   let imgURL;
+
+  const obj = {
+    id: imageId,
+    alt: imgName,
+    src: imgUrl,
+  };
+  const nameChangeHandler = (e) => {
+    setImgName(e.target.value);
+  };
+  const urlChangeHandler = (e) => {
+    setImgUrl(e.target.value);
+  };
+
+  const uploadHandler = () => {
+    jsonStr.push(obj);
+    console.log(jsonStr);
+    localStorage.setItem('gallery', JSON.stringify(obj));
+    setImgUrl('');
+    setImgName('');
+  };
+
+  return (
+    <div>
+      <label htmlFor='imgURL'>Insert your URL here:</label>
+      <input type='text' id='imgURL' value={imgUrl} onChange={urlChangeHandler} />
+      <label htmlFor='imgName'>Name</label>
+      <input type='text' id='imgName' value={imgName} onChange={nameChangeHandler} />
+      <button onClick={uploadHandler}>Upload</button>
+    </div>
+  );
+};
 
 export default UploadImage;
